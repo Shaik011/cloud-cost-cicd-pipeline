9580580ea439a064227e24d4f210dd1338d9b91f
Shaik AHmed <ahmedmohammed98670@gmail.com>
2026-09-03 16:29:28 +0530

    fix syntax error - remove duplicate closing brace

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -29,6 +29,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = "cloudcostcicd"
+  oidc_issuer_enabled = true
 
   default_node_pool {
     name       = "default"
@@ -42,8 +43,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id                    = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name            = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                             = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
